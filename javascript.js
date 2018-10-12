1f8a763ddc6d952c051c504504ddab06aa3539e2
Herman Sletmoen <hermansletmoen@gmail.com>
2018-10-12 21:14:07 +0200

    Use JavaScript built-in zero padding function

--- javascript.js
+++ javascript.js
@@ -9,35 +9,15 @@ function sendSubscription () {
 	req.send(encodeURI("email=" + "\"" + str + "\""));
 }
 
-// let stime = new Date(<?php echo microtime(true) * 1000 ?>);
-// let ctime = new Date();
-// let offset = stime.getTime() - ctime.getTime();
-// let prediction = new Date(<?php echo shell_exec("python3 light.py 2>&1") ?>);
-// document.getElementById("prediction").innerHTML = timeString(prediction, true);
-
 function updatePrediction() {
 	document.getElementById("prediction").innerHTML = timeString(prediction, true);
 }
 
-function pad2(n) {
-	if (n < 10) {
-		return "0" + n;
-	} else {
-		return n;
-	}
-}
-function pad3(n) {
-	if (n < 100) {
-		return "0" + pad2(n);
-	} else {
-		return n;
-	}
-}
 function timeString(dt, incl_ms) {
-	let h = pad2(dt.getHours());
-	let m = pad2(dt.getMinutes());
-	let s = pad2(dt.getSeconds());
-	let ms = pad3(dt.getMilliseconds());
+	let h = String(dt.getHours()).padStart(2, "0");
+	let m = String(dt.getMinutes()).padStart(2, "0");
+	let s = String(dt.getSeconds()).padStart(2, "0");
+	let ms = String(dt.getMilliseconds()).padStart(3, "0");
 	if (incl_ms) {
 		return h + ":" + m + ":" + s + "." + ms;
 	} else {
@@ -45,9 +25,9 @@ function timeString(dt, incl_ms) {
 	}
 }
 function dateString(dt, fully) {
-	let d = pad2(dt.getDate());
-	let m = pad2(dt.getMonth() + 1);
-	let y = pad2(dt.getFullYear());
+	let d = String(dt.getDate()).padStart(2, "0");
+	let m = String(dt.getMonth() + 1).padStart(2, "0");
+	let y = String(dt.getFullYear()).padStart(2, "0");
 	if (!fully) {
 		y = y % 100;
 	}
@@ -68,7 +48,6 @@ function updateTimer() {
 		document.getElementById("updatebutton").innerHTML = "Lysregistrering åpner om " + Math.floor(((prediction - 30000) - stime) / 1000) + " sekunder";
 	}
 }
-// updateTimer();
 setInterval(updateTimer, 101);
 
 var xhr = new XMLHttpRequest();
@@ -77,9 +56,7 @@ function updateServer () {
 	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4 && xhr.response === "Registrert.") {
-			//console.log(xhr.readyState, xhr.status, xhr.response);
 			location.reload();
-			//document.getElementById("errormessage").innerHTML = "success";
 		} else {
 			console.log(xhr.readyState, xhr.status, "\"", xhr.response, "\"");
 			document.getElementById("errormessage").innerHTML = xhr.response;
@@ -87,4 +64,3 @@ function updateServer () {
 	};
 	xhr.send();
 }
-//updateServer();
