2de76d3cc60d0d1dad1e986b337820c5dc963540
Herman Sletmoen <hermansletmoen@gmail.com>
2018-11-22 22:33:41 +0100

    Disabled mailing, separated mailing functionality

--- light.py
+++ light.py
@@ -13,57 +13,6 @@ args = ["", "", ""]
 for i in range(0, min(len(args), len(sys.argv[1:]))):
     args[i] = sys.argv[1+i]
 
-def mail(recipients, subject, text):
-    fromaddr = "fysikklandlyset@mail.com"
-    password = "Fysikkland123"
-    smtpaddr = "smtp.mail.com"
-    port = 587
-
-    server = smtplib.SMTP()
-    server.connect(smtpaddr, port)
-    server.login(fromaddr, password)
-
-    msg = MIMEText(text, "html")
-    msg["From"] = fromaddr
-    msg["To"] = ", ".join(recipients)
-    msg["Subject"] = subject
-
-    try:
-        server.send_message(msg)
-    except Exception as e:
-        print(e)
-        exit()
-
-    server.quit()
-
-def notify():
-    recipients = []
-    file = open("subscribers", "r")
-    for line in file:
-        email = line.strip()
-        recipients.append(email)
-    file.close()
-
-    subject = "Noen målte nettopp lyset på Fysikkland!"
-
-    text = ""
-    text += "Navigér til http://folk.ntnu.no/hermasl/light for å se målingen!\n"
-    text += "\n"
-    text += "Som medlem av Fysikklandlysets epostliste bidrar du til sikker arkivering av lysloggen gjennom et distribuert blockchain-inspirert sikkerhetskopieringssystem.\n"
-    text += "Du vil derfor finne en kopi av lysloggen under.\n"
-    text += "Fysikkland anmoder deg om å ta godt vare på kopien i tilfelle noe ufysikalsk inntreffer og den sentrale loggen tilintetgjøres helt eller delvis.\n"
-    text += "\n"
-    text += "Fysikkland ønsker deg en fin dag videre.\n"
-    text += "\n"
-    text += "--- START LYSLOGG SIKKERHETSKOPI ---\n"
-    file = open("light.dat", "r")
-    for line in file:
-        text += line
-    file.close()
-    text += "--- SLUTT LYSLOGG SIKKERHETSKOPI ---\n"
-
-    mail(recipients, subject, text)
-
 def linregr(x, y):
         assert len(x) == len(y), "different number of x and y values"
         n = len(x)
@@ -123,13 +72,6 @@ def log(str):
     file.write(line)
     file.close()
 
-def createconfirmationcode():
-    # random string
-    code = ""
-    for i in range(0, 30):
-        code += random.choice(string.ascii_lowercase)
-    return code
-
 if args[0] == "" or args[0] == "predict":
     epoch = datetime.datetime.utcfromtimestamp(0)
     print(int(getprediction().timestamp() * 1000))
@@ -146,7 +88,6 @@ elif args[0] == "add":
         line = now.strftime("%d.%m.%y %H:%M:%S\n")
         file.write(line)
         file.close()
-        notify()
 elif args[0] == "open":
     now = datetime.datetime.now()
     if registered(now):
@@ -161,85 +102,5 @@ elif args[0] == "list":
         millis = int((time - epoch).total_seconds() * 1000)
         print(millis)
     file.close()
-elif args[0] == "subscribe":
-    email = args[1]
-
-    # validate email
-    if not re.match("[^@]+@[^@]+\.[^@]+", email) or email.split()[0] != email:
-        print("Ugyldig formatert epostadresse.")
-        exit()
-
-    random.seed(time.time())
-    code = createconfirmationcode()
-    file = open("subscribers_unconfirmed", "r")
-    lines = file.readlines()
-    line = code + " " + email + "\n"
-    lines.append(line)
-    if len(lines) >= 1000: # only keep 1000 most recent subscription requests
-        lines = lines[len(lines)-1000:]
-    file.close()
-    file = open("subscribers_unconfirmed", "w")
-    file.writelines(lines)
-    file.close()
-
-    link = "http://folk.ntnu.no/hermasl/light/confirm_subscription.php"
-    link += "?email=" + email + "&code=" + code
-
-    subject = "Bekreftelse av varsling ved måling av lysslukking"
-    text = ""
-    text += "\"" + email + "\" har anmodet om å motta varsler hver gang lysslukking på Fysikkland måles.\n"
-    text += "For å bekrefte abonnementet, klikk <a href=\"" + link + "\">her</a>.\n"
-    mail([email], subject, text)
-    print("En bekreftelsesepost er sendt til " + email + ".")
-elif args[0] == "confirm":
-    email = args[1]
-    code = args[2]
-
-    file = open("subscribers_unconfirmed", "r")
-    lines = file.readlines()
-    file.close()
-
-    file = open("subscribers_unconfirmed", "w")
-    ok = False
-    for line in lines:
-        words = line.split()
-        if email == words[1] and code == words[0]:
-            ok = True
-        else:
-            file.write(line)
-    file.close()
-
-    if ok:
-        file = open("subscribers", "a")
-        line = email + "\n"
-        file.write(line)
-        file.close()
-        print(email + " bekreftet.")
-    else:
-        print(email + " ikke bekreftet.")
-elif args[0] == "unsubscribe":
-    print("Stengt.")
-    exit()
-
-    email = args[1]
-
-    file = open("subscribers", "r")
-    lines = file.readlines()
-    file.close()
-
-    found = False
-    file = open("subscribers", "w")
-    for line in lines:
-        if line.strip() == email:
-            found = True
-        else:
-            file.write(line)
-    file.close()
-
-    if found:
-        print("Abonnementet til " + email + " er avsluttet.")
-    else:
-        print("Fant ikke " + email + ".")
-        
 else:
     print("Ukjent kommando.")
