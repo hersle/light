af1fcb847a18648e4cf12d1d48754953e3e882f4
Herman Sletmoen <hermansletmoen@gmail.com>
2018-11-23 21:25:45 +0100

    Do not notify upon unsuccessful registration

--- post.php
+++ post.php
@@ -1,5 +1,7 @@
 <?php
 $output = exec(escapeshellcmd("./light.py add 2>&1"));
 echo "$output";
-exec("python3 mail.py notify > /dev/null 2>&1 &"); # mail in background
+if ($output == "Registrert.") {
+	exec("python3 mail.py notify > /dev/null 2>&1 &"); # mail in background
+}
 ?>
