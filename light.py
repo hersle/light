aadf43d4e1ca2b3c1a32493ae6ae9371ff87dea8
Herman Sletmoen <hermansletmoen@gmail.com>
2018-11-28 21:03:13 +0100

    Improved and reduced log messages

--- light.py
+++ light.py
@@ -25,37 +25,34 @@ if __name__ == "__main__":
     for i in range(0, min(len(args), len(sys.argv[1:]))):
         args[i] = sys.argv[1+i]
 
-    logging.info("called with arguments %s", args)
+    logging.info("executing \"%s\"", " ".join(sys.argv))
 
     if args[0] == "" or args[0] == "predict":
         epoch = datetime.datetime.utcfromtimestamp(0)
         ms = int(measure.getprediction().timestamp() * 1000)
         print(ms)
-        logging.info("predicted lights out at %d", ms)
     elif args[0] == "add":
         # add new time
         now = datetime.datetime.now()
         if measure.registered(now):
             print("Lys allerede registrert.")
-            logging.info("not registered - already registered")
+            logging.info("denied registration due to earlier registration today")
         elif not measure.timeisreasonable(now):
             print("Ufysikalsk tid. For stort avvik.")
-            logging.info("not registered - too large difference from prediction")
+            logging.info("denied registration due to large deviation from prediction")
         else:
             print("Registrert.")
             file = open("light.dat", "a")
             line = now.strftime("%d.%m.%y %H:%M:%S\n")
             file.write(line)
             file.close()
-            logging.info("successfully registered lights out at %s on machine \"%s\"", line.strip(), os.popen("uname -a").read().split()[1])
+            logging.info("successfully registered")
     elif args[0] == "open":
         now = datetime.datetime.now()
         if measure.registered(now):
             print("no")
-            logging.info("already registered")
         else:
             print("yes")
-            logging.info("light not registered")
     elif args[0] == "list":
         file = open("light.dat", "r")
         epoch = datetime.datetime.utcfromtimestamp(0)
@@ -64,22 +61,18 @@ if __name__ == "__main__":
             millis = int((time - epoch).total_seconds() * 1000)
             print(millis)
         file.close()
-        logging.info("succesfully listed previous measurements")
     elif args[0] == "time":
         ms = int(time.time() * 1000)
         print(ms)
-        logging.info("server time: %s ms (%s UTC) after epoch from \"%s\"", ms, datetime.datetime.utcfromtimestamp(int(ms / 1000)), os.popen("uname -a").read().split()[1])
     elif args[0] == "notify":
         mail.notify()
     elif args[0] == "subscribe":
         email = args[1]
 
-        logging.info("received subscription request from %s", email)
-
         # validate email
         if not re.match("[^@]+@[^@]+\.[^@]+", email) or email.split()[0] != email:
             print("Ugyldig formatert epostadresse.")
-            logging.info("denied subscription request - format error")
+            logging.info("denied subscription from %s due to format error", email)
             exit()
 
         random.seed(time.time())
@@ -104,12 +97,10 @@ if __name__ == "__main__":
         text += "For å bekrefte abonnementet, klikk <a href=\"" + link + "\">her</a>.\n"
         mail([email], subject, text)
         print("En bekreftelsesepost er sendt til " + email + ".")
-        logging.info("processed subscription request, attempted to send confirmation email")
+        logging.info("received subscription request from %s", email)
     elif args[0] == "confirm":
         email = args[1]
         code = args[2]
-
-        logging.info("received subscription confirmation request from %s", email)
 
         file = open("subscribers_unconfirmed", "r")
         lines = file.readlines()
@@ -131,10 +122,10 @@ if __name__ == "__main__":
             file.write(line)
             file.close()
             print(email + " bekreftet.")
-            logging.info("successfully confirmed subscription")
+            logging.info("confirmed subscription from %s", email)
         else:
             print(email + " ikke bekreftet.")
-            logging.info("subscription not confirmed - email not found or incorrect code")
+            logging.info("failed to confirm subscription from %s", email)
     elif args[0] == "unsubscribe":
         print("Stengt.")
         exit()
