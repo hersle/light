018bcede8d256fe35361d7a65815f8404f5cf05a
Herman Sletmoen <hermansletmoen@gmail.com>
2018-12-14 15:11:27 +0100

    Remove unnecessary piping of error output in registration request handler

--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 <?php
-$output = exec(escapeshellcmd("./light.py add 2>&1"));
+$output = exec(escapeshellcmd("./light.py add"));
 echo "$output";
 if ($output == "Slukking registrert.") {
 	exec("python3 light.py notify > /dev/null 2>&1 &"); # mail in background
